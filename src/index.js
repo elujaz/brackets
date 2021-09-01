bc418188ebe156c715ca469b9d3c34b82190ae63
elujaz <elujaz.i.a@gmail.com>
2021-09-01 21:07:15 +0300

    init split/join solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+
+  let stringsOfBrackets = bracketsConfig.map(pairOfBrackets => pairOfBrackets.join(''));
+
+  for (let i = 0; i < stringsOfBrackets.length; i++) {
+    if (str.split(stringsOfBrackets[i]).join('').length < str.length) {
+      str = str.split(stringsOfBrackets[i]).join('');
+      i = -1;
+    }
+  } 
+
+  return str ? false : true;
 }
